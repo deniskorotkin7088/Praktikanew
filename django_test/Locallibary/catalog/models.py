ea88217fdd9813f7dc45a30e994b35de19e53ae5
honor <deniskorotkin7088@gmail.com>
2025-11-17 23:23:46 +0700

    6 part

--- django_test/Locallibary/catalog/models.py
+++ django_test/Locallibary/catalog/models.py
@@ -43,6 +43,9 @@ class Author(models.Model):
         """
         return '%s, %s' % (self.last_name, self.first_name)
 
+    class Meta:
+        ordering = ['last_name']
+
 
 class Book(models.Model):
     """
@@ -51,6 +54,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13,blank=True, null=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
 
     def __str__(self):
--- django_test/Locallibary/catalog/views.py
+++ django_test/Locallibary/catalog/views.py
@@ -1,23 +1,35 @@
+from django.views import generic
+from .models import Book
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
-    # Генерация "количеств" некоторых главных объектов
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
-    # Доступные книги (статус = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()
 
-    # Отрисовка HTML-шаблона index.html с данными внутри
-    # переменной контекста context
     return render(
         request,
-        'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        'catalog/index.html',  # Исправленный путь
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors
+        },
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
