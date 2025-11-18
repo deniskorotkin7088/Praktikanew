ff4e658b1583732be8926abd783383ae8ded16f6
honor <deniskorotkin7088@gmail.com>
2025-11-18 16:19:56 +0700

    7 part

--- django_test/Locallibary/catalog/views.py
+++ django_test/Locallibary/catalog/views.py
@@ -1,5 +1,4 @@
 from django.views import generic
-from .models import Book
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 
@@ -7,16 +6,22 @@ def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    num_authors = Author.objects.count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+
+    # Render the HTML template index.html with the data in the context variable.
     return render(
         request,
-        'catalog/index.html',  # Исправленный путь
+        'catalog/index.html',
         context={
             'num_books': num_books,
             'num_instances': num_instances,
             'num_instances_available': num_instances_available,
-            'num_authors': num_authors
+            'num_authors': num_authors,
+            'num_visits': num_visits  # num_visits appended
         },
     )
 
