948468a5dce7c9225d93bb73eb2d11e83a4bca9b
deniskorotkin7088 <deniskorotkin7088@gmail.com>
2025-11-22 11:15:15 +0700

    New

--- pythonProject1/django_test/Locallibary/catalog/admin.py
+++ pythonProject1/django_test/Locallibary/catalog/admin.py
@@ -1,3 +1,97 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance
 
-# Register your models here.
+
+class BookInstanceInline(admin.TabularInline):
+    """Дополнительно: inline для отображения экземпляров книг в книге"""
+    model = BookInstance
+    extra = 0
+
+
+class BookInline(admin.TabularInline):
+    """Встроенный список книг для автора"""
+    model = Book
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    # Поля, которые будут отображаться в списке книг
+    list_display = ('title', 'display_author', 'display_genre')
+
+    # Поля для фильтрации
+    list_filter = ('genre', 'author')
+
+    # Поля для поиска
+    search_fields = ('title', 'author__first_name', 'author__last_name')
+
+    # Группировка полей в форме редактирования
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'author')
+        }),
+        ('Details', {
+            'fields': ('summary', 'genre')
+        }),
+    )
+
+    # Inline для отображения экземпляров книг
+    inlines = [BookInstanceInline]
+
+    # Предварительная загрузка связанных данных для оптимизации
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('author').prefetch_related('genre')
+
+    def display_author(self, obj):
+        return str(obj.author) if obj.author else 'Не указан'
+
+    def display_genre(self, obj):
+        genres = obj.genre.all()
+        if genres:
+            return ', '.join(genre.name for genre in genres)
+        return 'Не указаны'
+
+    display_author.short_description = 'Автор'
+    display_genre.short_description = 'Жанры'
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_filter = ('date_of_birth', 'date_of_death')
+    search_fields = ('last_name', 'first_name')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+    inlines = [BookInline]
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    # ОБЪЕДИНЕНО: представление списка BookInstance
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id_display')
+    list_filter = ('status', 'due_back')
+    search_fields = ('book__title', 'imprint')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+    def id_display(self, obj):
+        """Отображает ID книги"""
+        return str(obj.id)
+
+    id_display.short_description = 'ID экземпляра'
+
+
+# Регистрация моделей в админке
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Genre, GenreAdmin)
--- pythonProject1/django_test/Locallibary/catalog/models.py
+++ pythonProject1/django_test/Locallibary/catalog/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.urls import reverse
+from django.db.models import UniqueConstraint
+import uuid
+from django.contrib.auth.models import User
+from datetime import date
+
+
 class Genre(models.Model):
     """
     Model representing a book genre (e.g. Science Fiction, Non Fiction).
@@ -10,63 +17,13 @@ class Genre(models.Model):
         String for representing the Model object (in Admin site etc.)
         """
         return self.name
-from django.urls import reverse #Used to generate URLs by reversing the URL patterns
-
-class Book(models.Model):
-    """
-    Model representing a book (but not a specific copy of a book).
-    """
-    title = models.CharField(max_length=200)
-    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file.
-    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
-    isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-
-    def __str__(self):
-        """
-        String for representing the Model object.
-        """
-        return self.title
-
-
-    def get_absolute_url(self):
-        """
-        Returns the url to access a particular book instance.
-        """
-        return reverse('book-detail', args=[str(self.id)])
-import uuid # Required for unique book instances
-
-class BookInstance(models.Model):
-    """
-    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
-    """
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular book across whole library")
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-    imprint = models.CharField(max_length=200)
-    due_back = models.DateField(null=True, blank=True)
-
-    LOAN_STATUS = (
-        ('m', 'Maintenance'),
-        ('o', 'On loan'),
-        ('a', 'Available'),
-        ('r', 'Reserved'),
-    )
-
-    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
 
     class Meta:
-        ordering = ["due_back"]
+        constraints = [
+            UniqueConstraint(fields=['name'], name='genre_name_unique')
+        ]
 
 
-    def __str__(self):
-        """
-        String for representing the Model object
-        """
-        return '%s (%s)' % (self.id,self.book.title)
 class Author(models.Model):
     """
     Model representing an author.
@@ -82,9 +39,84 @@ class Author(models.Model):
         """
         return reverse('author-detail', args=[str(self.id)])
 
-
     def __str__(self):
         """
         String for representing the Model object.
         """
         return '%s, %s' % (self.last_name, self.first_name)
+
+    class Meta:
+        ordering = ['last_name']
+
+
+class Book(models.Model):
+    """
+    Model representing a book (but not a specific copy of a book).
+    """
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
+    isbn = models.CharField('ISBN', max_length=13,blank=True, null=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.title
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular book instance.
+        """
+        return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        """Создает строку для жанров. Это требуется для отображения в админке."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
+    def display_author(self):
+        """Возвращает строковое представление автора"""
+        return str(self.author) if self.author else 'No author'
+
+    display_author.short_description = 'Author'
+
+
+class BookInstance(models.Model):
+    """
+    Model representing a specific copy of a book (i.e. that can be borrowed from the library).
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular book across whole library")
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+    imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(max_length=1, choices=LOAN_STATUS, blank=True, default='m', help_text='Book availability')
+
+    class Meta:
+        ordering = ["due_back"]
+        permissions = (("can_mark_returned", "Set book as returned"),)
+
+    def __str__(self):
+        """
+        String for representing the Model object
+        """
+        book_title = self.book.title if self.book else 'Unknown Book'
+        return '%s (%s)' % (self.id, book_title)
